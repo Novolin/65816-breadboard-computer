e248939075ddebcd1b2ce3b7fb765f82924952e4
Novolin <55156472+Novolin@users.noreply.github.com>
2026-01-02 23:57:53 +0000

    Finished init code, I think?

--- Programs/includes/lcd.s
+++ Programs/includes/lcd.s
@@ -50,6 +50,8 @@ CURS_L4         = $54                   ; Same with line 4. 16x4 displays may ac
 
 DISP_BOOT:      ; Enable the VIA, initialize the LCD to 'default' mode.
  PHA                                    ; Hold on to whatever's in A.
+ PHY                                    ; And Y, we're going to use that for some other stuff.
+ LDY            #LCD_E                  ; Specifically toggling the "E" bit.
  LDA            #$FF
  STA            CHLCD::DDRB             ; Set up our DDRs, both all outputs
  STA            CHLCD::DDRA             
@@ -59,23 +61,51 @@ DISP_BOOT:      ; Enable the VIA, initialize the LCD to 'default' mode.
  LDA            #40                     ; 40 ms of delay, so the LCD can get up to stable voltage.
  JSR            delay                   ; Delay while the LCD wakes up
  LDA            #LCD_INIT               ; Prepare our init value
- INC            CHLCD::CMDPORT          ; Flag Enable bit
+ STY            CHLCD::CMDPORT          ; Flag Enable bit
  STA            CHLCD::DATAPORT         ; Get the data packet out
  DEC            CHLCD::CMDPORT          ; Latch it.
  LDA            #1                      ; do a short delay before re-sending
  JSR            delay                   ; I think it needs the init command 3x to deal with 4-bit mode?
- INC            CHLCD::CMDPORT          ; honestly, i'm just doing it because the datasheet does
+ STY            CHLCD::CMDPORT          ; honestly, i'm just doing it because the datasheet does
  LDA            #1                      ; re-loading the accum here so the pulse width isn't too short.
  DEC            CHLCD::CMDPORT          ; it would probably still be ok, but that's safety in case i boost past 1MHz
  JSR            delay                   ; last delay before we can be sure it stuck
- INC            CHLCD::CMDPORT          
- LDA            #LCD_ON_BLINK           ; choose your preferred power-on setting. I'm going with blinky for now
+ STY            CHLCD::CMDPORT          ; We need to wait after this so the pulse isn't too short.
+ NOP                                    ; Probably don't even need to, but we just waited like 50ms, what's 2us?
  DEC            CHLCD::CMDPORT          ; OK, now we can actually use the busy flag!!
+ JSR            LCD_WAIT                ; Loops until LCD is ready
+ STY            CHLCD::CMDPORT          ; LCD_WAIT will put the command port back to 0, so we just need the "E" bit to toggle.
+ LDA            #LCD_ON_BLINK           ; choose your preferred power-on setting. I'm going with blinky for now
+ STA            CHLCD::DATAPORT         ; Make sure it's on the bus
+ DEC            CHLCD::CMDPORT          ; Latch.
+ JSR            LCD_WAIT                ; Ensure it goes through.
+ STY            CHLCD::CMDPORT
+ LDA            #LCD_CLEAR              ; Send a clear command, too.
+ STA            CHLCD::DATAPORT         ; Launch it
+ DEC            CHLCD::CMDPORT          ; Latch it.
+ PLY                                    ; Return our original Y register
+ PLA                                    ; And our A register
+ RTS                                    ; And go back to what we were doing before!
+ 
+ 
+ 
 
-
- LCD_WAIT:      ; Wait for the LCD busy flag to clear
+LCD_WAIT:      ; Wait for the LCD busy flag to clear
  PHA                                    ; Keep A saved safely away
  LDA            #LCD_RW                 ; Prepare a read action
  STA            CHLCD::CMDPORT
-
+ LDA            #0                      ; Set our DDR to in on the data lines.
+ STA            CHLCD::DDRB             
+@READBUSY
+ INC            CHLCD::CMDPORT          ; Fire the Enable bit
+ LDA            CHLCD::DATAPORT         ; Load the byte
+ DEC            CHLCD::CMDPORT          ; Release E
+ AND            %10000000               ; Check if it's busy
+ BNE            @READBUSY               ; If it is, try again.
+ LDA            #$FF                    ; Busy flag cleared, turn the DDR back
+ STA            CHLCD::DDRB
+ LDA            #0  
+ STA            CHLCD::CMDPORT          ; Let us return to INC/DEC mode.
+ PLA                                    ; Don't forget the accumulator!
+ RTS                                    ; Return!
  
